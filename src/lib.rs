09db6cfb2cfa294b805f6a4e7ce9246d8155a127
David Barnett <david@barnett.net.nz>
2016-09-24 08:45:26 +1200

    feat(id3): add bare bones queryable implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,43 @@ extern crate id3;
 extern crate liquery;
 
 use liquery::Queryable;
+use id3::Tag;
+use std::path::Path;
+
+/// Queryable instace of a ID3 Tag
+pub struct QueryId3 {
+    tag: Tag, 
+}
+
+impl QueryId3 {
+
+    /// Create a QueryTag from a file
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, ()> {
+        // TODO: return a proper error.
+        match Tag::read_from_path(path) {
+            Ok(tag) => Ok(QueryId3 {
+                tag: tag,
+            }),
+            Err(_) => Err(()),
+        }
+    }
+
+}
+
+impl Queryable for QueryId3 {
+
+    fn query(&self, key: &str) -> Option<String> {
+        // TODO: populate the key, field pairs
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::QueryId3;
+
+    #[test]
+    fn fails_on_non_existing_file() {
+        assert!(QueryId3::new("non-existing-file").is_err()); 
+    } 
+}
